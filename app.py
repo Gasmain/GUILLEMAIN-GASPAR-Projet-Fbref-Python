1d70f8a1be14ed9e7507849a9b3296ded986d355
Gaspar <gasparguillemain@gmail.com>
2022-11-26 10:58:54 +0100

    tailwind dash design

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ import numpy as np
 import dash
 import plotly.express as px
 import dash_bootstrap_components as dbc
+from dash import Output, Input
 from flask.helpers import get_root_path
 
 PLAYER_FILE_JSON = "data/player.json"
 PLAYER_FILE_CSV = "data/player.csv"
-MY_LOGO = "r'assets/soccer_logo.png"
+MY_LOGO = "assets/logo.png"
 
 
 #TODO : Shoot, Pass, deffense, dribble
@@ -36,39 +37,27 @@ def create():
         "padding": "2rem 2rem",
     }
 
-    CONTENT_STYLE = {
-        "margin-top": "20rem",
-        "padding": "2rem 2rem",
-    }
 
     navbar = dbc.NavbarSimple(
         children=[
-            dash.html.A(
-                # Use row and col to control vertical alignment of logo / brand
-                dbc.Row(
-                    [
-                        dbc.Col(dash.html.Img(src=MY_LOGO, height="30px")),
-                        dbc.Col(dbc.NavbarBrand("Navbar", className="ms-2")),
-                    ],
-                    align="center",
-                    className="g-0",
-                ),
-                href="/"
-            ),
             dbc.NavLink([dash.html.I(className="bi bi-house-door-fill", style={"margin-right": "10px"}), "Dashboard"],
                         href="/", active="exact"),
             dbc.NavLink([dash.html.I(className="bi bi-person-fill", style={"margin-right": "10px"}), "Players"],
                         href="/players", active="exact"),
-            dbc.NavLink([dash.html.I(className="bi bi-download", style={"margin-right": "10px"}), "Scrapping"],
+            dbc.NavLink([dash.html.I(className="bi bi-cloud-arrow-down-fill", style={"margin-right": "10px"}), "Scrapping"],
                         href="/scrapping", active="exact")
         ],
         color="dark",
         dark=True,
+        brand="Dash",
+        brand_href="/",
+        brand_style={"color":"#6265F0", "font-weight" : "700", "font-size":"25px"}
     )
 
 
 
-    content = dash.html.Div([dash.page_container],id="pages-content", style=CONTENT_STYLE)
+    content = dash.html.Div([dash.page_container],id="pages-content")
+
 
     app.layout = dash.html.Div([dash.dcc.Location(id="url"), navbar, content])
 
@@ -80,4 +69,5 @@ def build_data_frame():
     data = json.load(f)
     df = pd.json_normalize(data)
     return df
+
 
--- pages/home.py
+++ pages/home.py
@@ -4,6 +4,9 @@ from dash import html, dcc
 dash.register_page(__name__, path='/')
 
 layout = html.Div(children=[
+
+    html.Div([dash.html.H2(children='Dashboard')], style={"width": "100%", "height" : "87px", "background-color":"white"}),
+
     html.H1(children='This is our Home page'),
 
     html.Div(children='''
